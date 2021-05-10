f3cd50ea6dd245d26daf2c2ce5d6a7278dbdf16b
hai.lengoc <hai.lengoc@pycogroup.com>
2021-05-10 20:07:05 +0700

    dpeloy

--- src/constant/route/route.js
+++ src/constant/route/route.js
@@ -1,17 +1,17 @@
-//Clinic
+
 export const ROUTE_CLINICLIST_LIST = '/clinicList';
 export const ROUTE_CLINIC_DETAIL = '/profile/patient/clinics/detail/:clinicId';
 export const ROUTE_CLINIC_DETAIL_BUILD = (clinicId) => `/profile/patient/clinics/detail/${clinicId}`;
 
-///News
+
 export const ROUTE_NEWS_LIST = '/news';
 export const ROUTE_NEWS_DETAIL = `/news/detail/:newsId`;
 export const ROUTE_NEWS_DETAIL_BUILD = (newsId) => `/news/detail/${newsId}`;
 
-//Sign in
+
 export const ROUTE_SIGN_IN = '/signin';
 
-//Sign Out
+
 export const ROUTE_SIGN_UP = '/signup';
 
 export const ROUTE_MY_PROFILE = '/profile';
